ab2fe01d09a303ce7078179e7fc2708773d0e97e
Mobile Making <mm@Alexs-MacBook-Pro-2.local>
2014-10-23 13:42:36 -0700

    Initial Commit

---
+++ Week1FooBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Week1FooBar
+//
+//  Created by Mobile Making on 10/23/14.
+//  Copyright (c) 2014 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
